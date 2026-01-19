ac4b9835a4986faa2baff0f4fcf883b2d0af0a77
arthurp1910 <137120697+arthurp1910@users.noreply.github.com>
2026-01-19 14:03:15 -0300

    Update no renderizador

--- Trabalho CG - V.Final/src/core/renderizador.py
+++ Trabalho CG - V.Final/src/core/renderizador.py
@@ -18,26 +18,22 @@ def _u1i(prog, name, x):
         glUniform1i(loc, int(x))
 
 def desenhar(vao_tuple, model, vp, prog,
-             # camera
              view_pos=(0.0, 0.0, 5.0),
 
-             # direcional
+         
              dir_dir=(0.3, -1.0, 0.2),
              dir_color=(1.0, 1.0, 1.0),
              dir_intensity=1.0,
              ambient_strength=0.18,
-
-             # 4 point lights
+          
              point_pos=None,
              point_color=None,
              point_intensity=None,
              point_range=None,
 
-             # material
              tint=None,
              unlit=False,
-
-             # textura
+            
              tex=None,
              use_tex=False):
 
@@ -67,29 +63,20 @@ def desenhar(vao_tuple, model, vp, prog,
     glUniformMatrix4fv(glGetUniformLocation(prog, "mvp"), 1, GL_FALSE, mvp.T)
     glUniformMatrix4fv(glGetUniformLocation(prog, "model"), 1, GL_FALSE, model.T)
 
-    # material tint
     _u3(prog, "uTint", tint)
-
-    # camera
     _u3(prog, "viewPos", view_pos)
-
-    # unlit
     _u1i(prog, "uUnlit", 1 if unlit else 0)
 
-    # direcional
     _u3(prog, "uDirLightDir", dir_dir)
     _u3(prog, "uDirLightColor", dir_color)
     _u1f(prog, "uDirLightIntensity", dir_intensity)
     _u1f(prog, "uAmbientStrength", ambient_strength)
 
-    # 4 point lights (arrays)
     for i in range(NUM_POINT_LIGHTS):
         _u3(prog, f"uPointPos[{i}]", point_pos[i])
         _u3(prog, f"uPointColor[{i}]", point_color[i])
         _u1f(prog, f"uPointIntensity[{i}]", point_intensity[i])
         _u1f(prog, f"uPointRange[{i}]", point_range[i])
-
-    # -------- TEXTURA (opcional) --------
     _u1i(prog, "uUseTex", 1 if (use_tex and tex is not None) else 0)
 
     if use_tex and tex is not None:
@@ -102,4 +89,5 @@ def desenhar(vao_tuple, model, vp, prog,
 
     glBindVertexArray(vao)
     glDrawElements(GL_TRIANGLES, count, GL_UNSIGNED_INT, None)
+
     glBindVertexArray(0)
